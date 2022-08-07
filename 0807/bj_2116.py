375813bd1039f460fa34b36289889ef20a9942d4
GyeongSam <rud7tka@gmail.com>
2022-08-08 01:28:11 +0900

    0807 공부흔적

--- 0807/bj_2116.py
+++ 0807/bj_2116.py
@@ -1,22 +1,21 @@
-def opp(n,i):
-    sub=[L[i].index(n)]
-    if sub==0:
-        return L[i][5]
-    elif sub==1:
-        return L[i][5]
-    elif sub==2:
-        return L[i][5]
-    elif sub==3:
-        return L[i][5]
-    elif sub==4:
-        return L[i][5]
-    elif sub==5:
-        return L[i][5]
+o=[5,3,4,1,2,0]
     
-
-
+def opp(n):
+    return o[n]
+def opp_num(n,i):
+    return L[i][opp(L[i].index(n))]
+def max_num(n,i):
+    return max([x for x in L[i] if x!=n and x!=opp_num(n,i)])
 
 L=[list(map(int,input().split())) for _ in range(int(input()))]
-s=[]
-S6,S5=L[0].index(6),L[0].index(5)
+
+s=[0]*6
+t=[1,2,3,4,5,6]
+
+for i in range(len(L)):
+    for j in range(6):
+        s[j]+=max_num(t[j],i)
+        t[j]=opp_num(t[j],i)
+
+print(max(s))        
 
