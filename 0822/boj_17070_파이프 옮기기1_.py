8ba6c09a290258e1b07866f3b327812229811832
GyeongSam <rud7tka@gmail.com>
2022-08-23 00:30:12 +0900

    0822 공부흔적

--- 0822/boj_17070_파이프 옮기기1_.py
+++ 0822/boj_17070_파이프 옮기기1_.py
@@ -4,14 +4,19 @@ stack=[(1,2,0)]
 cnt=0
 while stack!=[]:
     I,J,d=stack.pop()
-    if I+1==N or J+1==N
-
-
-        stack.append((I+1,J+1,1))
-    if d!=2 and check(0):
-        if isN(0,1):cnt+=1
-        else:stack.append((I,J+1,0))
-    if d!=0 and check(2):
-        if isN(1,0):cnt+=1
-        else:stack.append((I+1,J,2))
+    sub=[]
+    I2=I+1
+    J2=J+1
+    sub.append(Room[I][J2])
+    sub.append(Room[I2][J2])
+    sub.append(Room[I2][J])
+    if sub==[0,0,0]:
+        if J2==N and I2==N:cnt+=1
+        else:stack.append((I2,J2,1))
+    if d!=2 and sub[0]==0:
+        if I==N and J2==N:cnt+=1
+        else:stack.append((I,J2,0))
+    if d!=0 and sub[2]==0:
+        if I2==N and J==N:cnt+=1
+        else:stack.append((I2,J,2))
 print(cnt)
