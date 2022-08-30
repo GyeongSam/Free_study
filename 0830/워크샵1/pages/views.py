1f3e81eaa3d89ada1d1ef7823a247843b6746e30
GyeongSam <rud7tka@gmail.com>
2022-08-30 22:42:29 +0900

    0830 잘못업로드 삭제

--- 0830/워크샵1/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def dinner(request, food, num):
-    context={
-        'food':food,
-        'num':num,
-    }
-    return render(request, 'dinner.html',context)
